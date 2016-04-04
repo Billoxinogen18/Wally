27b267512028641db77a680feef16980224ea71b
Gio <ggogi12@freeuni.edu.ge>
2016-04-04 22:28:28 +0400

    Implement markers functionality

--- app/src/main/java/com/wally/wally/dal/Content.java
+++ app/src/main/java/com/wally/wally/dal/Content.java
@@ -1,5 +1,10 @@
 package com.wally.wally.dal;
 
+import com.google.android.gms.maps.model.LatLng;
+
 public abstract class Content {
 
+    public LatLng getLatLngLocation() {
+        return null;
+    }
 }
