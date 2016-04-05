207b045bb60836f3f5418977c5778843319bdbd5
Ioane Sharvadze <ioane.sharvadze@gmail.com>
2016-04-05 16:41:30 +0400

    Merge

--- app/src/main/java/com/wally/wally/activities/MapsActivity.java
+++ app/src/main/java/com/wally/wally/activities/MapsActivity.java
@@ -8,7 +8,6 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.ActivityCompat;
-import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
@@ -32,11 +31,13 @@ import com.wally.wally.dal.Callback;
 import com.wally.wally.dal.Content;
 import com.wally.wally.dal.DataAccessLayer;
 import com.wally.wally.dal.LatLngBoundsQuery;
-import com.wally.wally.dal.Query;
 
 import java.util.Collection;
 
-public class MapsActivity extends AppCompatActivity implements OnMapReadyCallback, GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener, GoogleMap.OnCameraChangeListener {
+public class MapsActivity extends AppCompatActivity implements OnMapReadyCallback,
+        GoogleApiClient.ConnectionCallbacks,
+        GoogleApiClient.OnConnectionFailedListener,
+        GoogleMap.OnCameraChangeListener {
 
     public static final String TAG = MapsActivity.class.getSimpleName();
 
@@ -44,8 +45,7 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
     private GoogleMap mMap;
     private SupportMapFragment mapFragment;
     private GoogleApiClient mGoogleApiClient;
-
-    private Content[] contents;
+    private Content[] mContents;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -67,7 +67,7 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
 
-        FloatingActionButton findMyLocation  = (FloatingActionButton) findViewById(R.id.my_location);
+        FloatingActionButton findMyLocation = (FloatingActionButton) findViewById(R.id.my_location);
         assert findMyLocation != null; // TODO WTF
         findMyLocation.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -158,42 +158,43 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
     }
 
     // TODO extract to parent
-    private DataAccessLayer getDAL(){
-        App app = (App)getApplicationContext();
+    private DataAccessLayer getDAL() {
+        App app = (App) getApplicationContext();
         return app.getDal();
     }
 
-    private void showContents(){
+    private void showContents() {
         mMap.clear();
-        for (Content content : contents) {
+        for (Content content : mContents) {
             LatLng location = content.getLatLngLocation();
             mMap.addMarker(new MarkerOptions().position(location).title("Hello World"));
         }
     }
+
     @Override
     public void onCameraChange(CameraPosition cameraPosition) {
         Log.d(TAG, cameraPosition.toString());
-        if(cameraPosition.zoom > 15) {
+        if (cameraPosition.zoom > 15) {
             LatLngBounds bounds = mMap.getProjection().getVisibleRegion().latLngBounds;
             DataAccessLayer dal = getDAL();
             dal.fetch(new LatLngBoundsQuery(bounds), new Callback<Collection<Content>>() {
                 @Override
                 public void call(@NonNull Collection<Content> result, @Nullable Exception e) {
                     if (e == null) {
-                        contents = new Content[result.size()];
+                        mContents = new Content[result.size()];
                         int i = 0;
                         for (Content content : result) {
-                            contents[i++] = content;
+                            mContents[i++] = content;
                         }
                         showContents();
                     } else {
                         Log.e(TAG, e.getMessage(), e);
-                        Toast.makeText(getApplicationContext(), getString(R.string.dal_fetch_failed),
+                        Toast.makeText(getApplicationContext(), getString(R.string.error_fetch_failed),
                                 Toast.LENGTH_LONG).show();
                     }
                 }
             });
-        }else{
+        } else {
             // Clear map on small zoom
             mMap.clear();
         }
