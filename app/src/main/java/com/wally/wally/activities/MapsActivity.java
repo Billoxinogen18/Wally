9e3044ebb95788f9d2224e8982c15f361d111187
Gio <ggogi12@freeuni.edu.ge>
2016-04-07 02:59:08 +0400

    Fix zoomout issue on markers

--- app/src/main/java/com/wally/wally/activities/MapsActivity.java
+++ app/src/main/java/com/wally/wally/activities/MapsActivity.java
@@ -6,7 +6,6 @@ import android.location.Location;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
-import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
@@ -44,11 +43,10 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
         GoogleApiClient.OnConnectionFailedListener,
         GoogleMap.OnCameraChangeListener {
 
-    public static final String TAG = MapsActivity.class.getSimpleName();
-
+    private static final String TAG = MapsActivity.class.getSimpleName();
     private static final int MY_LOCATION_REQUEST_CODE = 22;
+
     private GoogleMap mMap;
-    private SupportMapFragment mapFragment;
     private GoogleApiClient mGoogleApiClient;
     private Set<Content> mContents;
     private Map<Content, Marker> mMarkers;
@@ -69,23 +67,99 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
                     .addApi(LocationServices.API)
                     .build();
         }
-
-
+        
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-        mapFragment = (SupportMapFragment) getSupportFragmentManager()
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
-
-        FloatingActionButton findMyLocation = (FloatingActionButton) findViewById(R.id.my_location);
-        assert findMyLocation != null; // TODO WTF
-        findMyLocation.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                centerMapOnMyLocation();
-            }
-        });
     }
 
+    public void onMyLocationClick(View v) {
+        centerMapOnMyLocation();
+    }
+
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+        mMap.setOnCameraChangeListener(this);
+        centerMapOnMyLocation();
+    }
+
+    @Override
+    public void onCameraChange(CameraPosition cameraPosition) {
+        Log.d(TAG, cameraPosition.toString());
+        if (cameraPosition.zoom > 15) {
+            App app = (App) getApplicationContext();
+            DataAccessLayer dal = app.getDal();
+
+            LatLngBounds bounds = mMap.getProjection().getVisibleRegion().latLngBounds;
+            markersSetVisible(true);
+
+            dal.fetch(new LatLngBoundsQuery(bounds), new Callback<Collection<Content>>() {
+                @Override
+                public void call(Collection<Content> result, @Nullable Exception e) {
+                    if (e == null) {
+                        mContents.clear();
+                        mContents.addAll(result);
+                        showContents();
+                    } else {
+                        Log.e(TAG, e.getMessage(), e);
+                        Toast.makeText(getApplicationContext(), getString(R.string.error_fetch_failed),
+                                Toast.LENGTH_LONG).show();
+                    }
+                }
+            });
+        } else {
+            markersSetVisible(false);
+        }
+    }
+
+    private void showContents() {
+        for (Content cur : mMarkers.keySet())
+            if (!mContents.contains(cur))
+                mMarkers.remove(cur);
+
+        for (Content content : mContents) {
+            if (!mMarkers.keySet().contains(content)) {
+                Marker m = mMap.addMarker(new MarkerOptions()
+                        .position(content.getLatlng())
+                        .title("Hello World")
+                );
+                mMarkers.put(content, m);
+            }
+        }
+    }
+
+    private void markersSetVisible(boolean visible) {
+        for (Marker m : mMarkers.values()) {
+            m.setVisible(visible);
+        }
+    }
+
+
+    private void centerMapOnMyLocation() {
+        if (Utils.checkLocationPermission(this)) {
+            if (!mMap.isMyLocationEnabled()) {
+                mMap.setMyLocationEnabled(true);
+                mMap.getUiSettings().setMyLocationButtonEnabled(false);
+            }
+
+            Location myLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+
+            if (myLocation == null) {
+                Log.e(TAG, "centerMapOnMyLocation: couldn't get user location");
+                return;
+            }
+            LatLng myPosition = new LatLng(myLocation.getLatitude(), myLocation.getLongitude());
+
+            mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(myPosition, 16), 2000, null);
+        } else {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                    MY_LOCATION_REQUEST_CODE);
+        }
+    }
 
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String permissions[],
@@ -96,48 +170,6 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
                     grantResults[0] == PackageManager.PERMISSION_GRANTED) {
                 centerMapOnMyLocation();
             }
-        }
-    }
-
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
-    @Override
-    public void onMapReady(GoogleMap googleMap) {
-        mMap = googleMap;
-        mMap.setMyLocationEnabled(true);
-        mMap.getUiSettings().setMyLocationButtonEnabled(false);
-        if (Utils.checkLocationPermission(this)) {
-            centerMapOnMyLocation();
-        } else {
-            ActivityCompat.requestPermissions(this,
-                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
-                    MY_LOCATION_REQUEST_CODE);
-        }
-
-        mMap.setOnCameraChangeListener(this);
-    }
-
-    private void centerMapOnMyLocation() {
-        try {
-            Location myLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
-
-            if (myLocation == null) {
-                Log.e(TAG, "centerMapOnMyLocation: couldn't get user location");
-                return;
-            }
-            LatLng myPosition = new LatLng(myLocation.getLatitude(), myLocation.getLongitude());
-
-            mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(
-                    myPosition, 16), 2000, null);
-        } catch (SecurityException e) {
-            e.printStackTrace();
         }
     }
 
@@ -164,57 +196,5 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
     @Override
     public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
 
-    }
-
-    // TODO extract to parent
-    private DataAccessLayer getDAL() {
-        App app = (App) getApplicationContext();
-        return app.getDal();
-    }
-
-    private void showContents() {
-
-
-        for (Content cur : mMarkers.keySet()) {
-            if (!mContents.contains(cur)) {
-                mMarkers.remove(cur);
-            }
-        }
-
-        for (Content content : mContents) {
-            LatLng location = content.getLatlng();
-            if (!mMarkers.keySet().contains(content)) {
-                Marker m = mMap.addMarker(new MarkerOptions().position(location).title("Hello World"));
-                mMarkers.put(content, m);
-            }
-        }
-
-
-    }
-
-    @Override
-    public void onCameraChange(CameraPosition cameraPosition) {
-        Log.d(TAG, cameraPosition.toString());
-        if (cameraPosition.zoom > 15) {
-            LatLngBounds bounds = mMap.getProjection().getVisibleRegion().latLngBounds;
-            DataAccessLayer dal = getDAL();
-            dal.fetch(new LatLngBoundsQuery(bounds), new Callback<Collection<Content>>() {
-                @Override
-                public void call(@NonNull Collection<Content> result, @Nullable Exception e) {
-                    if (e == null) {
-                        mContents.clear();
-                        mContents.addAll(result);
-                        showContents();
-                    } else {
-                        Log.e(TAG, e.getMessage(), e);
-                        Toast.makeText(getApplicationContext(), getString(R.string.error_fetch_failed),
-                                Toast.LENGTH_LONG).show();
-                    }
-                }
-            });
-        } else {
-            // Clear map on small zoom
-            mMap.clear();
-        }
     }
 }
