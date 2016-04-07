c0fd1d551f9a27e5616c6ed070ee8936645bf98e
Viper <mkenc12@freeuni.edu.ge>
2016-04-07 14:04:55 +0400

    Implement save methods in FirebaseDAL for #2

--- app/src/main/java/com/wally/wally/dal/FirebaseDAL.java
+++ app/src/main/java/com/wally/wally/dal/FirebaseDAL.java
@@ -2,11 +2,14 @@ package com.wally.wally.dal;
 
 import android.content.Context;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.util.Log;
 
 import com.firebase.client.DataSnapshot;
 import com.firebase.client.Firebase;
 import com.firebase.client.FirebaseError;
 import com.firebase.client.ValueEventListener;
+import com.google.android.gms.maps.model.LatLng;
 
 import java.util.Collection;
 import java.util.HashSet;
@@ -18,12 +21,18 @@ public class FirebaseDAL implements DataAccessLayer<Content> {
     public FirebaseDAL(Context context) {
         Firebase.setAndroidContext(context);
         fb = new Firebase("https://burning-inferno-2566.firebaseio.com/").child("Contents");
+        save(new Content(new LatLng(0, 0)));
     }
 
     @Override
-    public void save(@NonNull Content c, @NonNull Callback<Boolean> statusCallback) {
-        // TODO
-        statusCallback.call(false, new UnsupportedOperationException("Not implemented"));
+    public void save(@NonNull Content c, @NonNull final Callback<Boolean> statusCallback) {
+        fb.push().setValue(c, new Firebase.CompletionListener() {
+            @Override
+            public void onComplete(FirebaseError firebaseError, Firebase firebase) {
+                boolean status = firebaseError == null;
+                statusCallback.call(status, status ? null : firebaseError.toException());
+            }
+        });
     }
 
     @Override
