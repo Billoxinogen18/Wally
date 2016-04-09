7deb22dda798c2e47aa501a546092a3dff9c4235
Ioane Sharvadze <ioane.sharvadze@gmail.com>
2016-04-09 13:03:50 +0400

    Fix MultidexingOn pre lollipop devices application was crashing

--- app/src/main/java/com/wally/wally/App.java
+++ app/src/main/java/com/wally/wally/App.java
@@ -1,22 +1,34 @@
 package com.wally.wally;
 
 import android.app.Application;
+import android.content.Context;
+import android.support.multidex.MultiDex;
 
 import com.wally.wally.dal.DALFactory;
 import com.wally.wally.dal.DataAccessLayer;
 
 /**
+ * Application class for Application wide feature initializations.
+ * <p/>
  * Created by ioane5 on 3/31/16.
  */
 public class App extends Application {
     private DataAccessLayer dal;
+
     @Override
     public void onCreate() {
         super.onCreate();
-        dal  = DALFactory.create(this);
+        dal = DALFactory.create(this);
     }
 
     public DataAccessLayer getDal() {
         return dal;
+    }
+
+    // support multi-dex.
+    @Override
+    protected void attachBaseContext(Context base) {
+        super.attachBaseContext(base);
+        MultiDex.install(this);
     }
 }
