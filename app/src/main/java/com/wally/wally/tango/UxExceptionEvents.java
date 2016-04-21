dd2ed52e85c37f71956f943a66cd4aaf4b8d8fc9
iomodo <wineson@gmail.com>
2016-04-21 19:48:50 +0400

    added error logs in UxExceptionEvents

--- app/src/main/java/com/wally/wally/tango/UxExceptionEvents.java
+++ app/src/main/java/com/wally/wally/tango/UxExceptionEvents.java
@@ -1,5 +1,7 @@
 package com.wally.wally.tango;
 
+import android.util.Log;
+
 import com.google.atap.tango.ux.UxExceptionEvent;
 import com.google.atap.tango.ux.UxExceptionEventListener;
 
@@ -7,6 +9,8 @@ import com.google.atap.tango.ux.UxExceptionEventListener;
  * Created by shota on 4/21/16.
  */
 public class UxExceptionEvents implements UxExceptionEventListener {
+    private static final String TAG = UxExceptionEvents.class.getSimpleName();
+
     private static UxExceptionEvents ourInstance = new UxExceptionEvents();
 
     public static UxExceptionEvents getInstance() {
@@ -18,25 +22,33 @@ public class UxExceptionEvents implements UxExceptionEventListener {
 
     @Override
     public void onUxExceptionEvent(UxExceptionEvent uxExceptionEvent) {
-        switch (uxExceptionEvent.getType()) {
-            case UxExceptionEvent.TYPE_OVER_EXPOSED:
-                break;
-            case UxExceptionEvent.TYPE_UNDER_EXPOSED:
-                break;
-            case UxExceptionEvent.TYPE_MOVING_TOO_FAST:
-                break;
-            case UxExceptionEvent.TYPE_FEW_FEATURES:
-                break;
-            case UxExceptionEvent.TYPE_FEW_DEPTH_POINTS:
-                break;
-            case UxExceptionEvent.TYPE_LYING_ON_SURFACE:
-                break;
-            case UxExceptionEvent.TYPE_MOTION_TRACK_INVALID:
-                break;
-            case UxExceptionEvent.TYPE_TANGO_SERVICE_NOT_RESPONDING:
-                break;
-            case UxExceptionEvent.TYPE_INCOMPATIBLE_VM:
-                break;
+        if(uxExceptionEvent.getType() == UxExceptionEvent.TYPE_LYING_ON_SURFACE){
+            Log.i(TAG, "Device lying on surface ");
         }
+        if(uxExceptionEvent.getType() == UxExceptionEvent.TYPE_FEW_DEPTH_POINTS){
+            Log.i(TAG, "Very few depth points in mPoint cloud " );
+        }
+        if(uxExceptionEvent.getType() == UxExceptionEvent.TYPE_FEW_FEATURES){
+            Log.i(TAG, "Invalid poses in MotionTracking ");
+        }
+        if(uxExceptionEvent.getType() == UxExceptionEvent.TYPE_INCOMPATIBLE_VM){
+            Log.i(TAG, "Device not running on ART");
+        }
+        if(uxExceptionEvent.getType() == UxExceptionEvent.TYPE_MOTION_TRACK_INVALID){
+            Log.i(TAG, "Invalid poses in MotionTracking ");
+        }
+        if(uxExceptionEvent.getType() == UxExceptionEvent.TYPE_MOVING_TOO_FAST){
+            Log.i(TAG, "Invalid poses in MotionTracking ");
+        }
+        if(uxExceptionEvent.getType() == UxExceptionEvent.TYPE_OVER_EXPOSED){
+            Log.i(TAG, "Camera Over Exposed");
+        }
+        if(uxExceptionEvent.getType() == UxExceptionEvent.TYPE_TANGO_SERVICE_NOT_RESPONDING){
+            Log.i(TAG, "TangoService is not responding ");
+        }
+        if(uxExceptionEvent.getType() == UxExceptionEvent.TYPE_UNDER_EXPOSED){
+            Log.i(TAG, "Camera Under Exposed " );
+        }
+
     }
 }
