cf8ed766810c81bfd6f5672fa54f35e278ec10f9
iomodo <wineson@gmail.com>
2016-04-29 14:35:51 +0400

    Add test tools

---
+++ app/src/test/java/wallyTest/UtilsTest.java
@@ -0,0 +1,13 @@
+package wallyTest;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public final class UtilsTest {
+    @Test
+    public void test1(){
+        assertTrue(true);
+
+    }
+
+}
