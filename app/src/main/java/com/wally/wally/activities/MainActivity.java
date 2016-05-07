303d35c75590c716cb818d832d708298171704ff
Ioane Sharvadze <ioane.sharvadze@gmail.com>
2016-05-07 13:28:29 +0400

    Add Plus Sign in
    Added todos for laster integration

--- app/src/main/java/com/wally/wally/activities/MainActivity.java
+++ app/src/main/java/com/wally/wally/activities/MainActivity.java
@@ -16,17 +16,26 @@
 
 package com.wally.wally.activities;
 
+import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.Intent;
-import android.graphics.Bitmap;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.OptionalPendingResult;
+import com.google.android.gms.common.api.ResultCallback;
 import com.google.atap.tango.ux.TangoUxLayout;
 import com.google.atap.tangoservice.Tango;
 import com.google.atap.tangoservice.TangoPoseData;
@@ -54,10 +63,11 @@ import java.util.List;
 
 public class MainActivity extends AppCompatActivity implements
         NewContentDialogFragment.NewContentDialogListener,
-        ContentFitter.FittingStatusListener, ContentSelectListener {
+        ContentFitter.FittingStatusListener, ContentSelectListener, GoogleApiClient.OnConnectionFailedListener {
 
     private static final String TAG = MainActivity.class.getSimpleName();
     private static final String ARG_ADF_UUID = "ARG_ADF_UUID";
+    private static final int REQUEST_CODE_SIGN_IN = 128;
 
     private TangoManager mTangoManager;
     private ContentFitter mContentFitter;
@@ -69,6 +79,8 @@ public class MainActivity extends AppCompatActivity implements
 
     private Content mSelectedContent;
     private long mLastSelectTime;
+    private GoogleApiClient mGoogleApiClient;
+    private ProgressDialog mProgressDialog;
 
     /**
      * Returns intent to start main activity.
@@ -82,6 +94,7 @@ public class MainActivity extends AppCompatActivity implements
         return i;
     }
 
+    @SuppressWarnings("ConstantConditions")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -110,6 +123,99 @@ public class MainActivity extends AppCompatActivity implements
                 mContentFitter = new ContentFitter(c, mTangoManager);
             }
             onContentSelected((Content) savedInstanceState.getSerializable("mSelectedContent"));
+        }
+
+        // TODO refactor WITH GioGoG
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .requestId()
+                .build();
+
+        // Build a GoogleApiClient with access to the Google Sign-In API and the
+        // options specified by gso.
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .enableAutoManage(this /* FragmentActivity */, this /* OnConnectionFailedListener */)
+                .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
+                .build();
+
+        findViewById(R.id.btn_google_sign_in).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onGoogleSignInClicked();
+            }
+        });
+        // silent sign in.
+        OptionalPendingResult<GoogleSignInResult> pendingResult =
+                Auth.GoogleSignInApi.silentSignIn(mGoogleApiClient);
+        if (pendingResult.isDone()) {
+            // There's immediate result available.
+            handleSignInResult(pendingResult.get());
+        } else {
+            findViewById(R.id.btn_new_post).setVisibility(View.GONE);
+            // There's no immediate result ready, displays some progress indicator and waits for the
+            // async callback.
+            showProgressDialog();
+            pendingResult.setResultCallback(new ResultCallback<GoogleSignInResult>() {
+                @Override
+                public void onResult(@NonNull GoogleSignInResult result) {
+                    handleSignInResult(result);
+                    hideProgressDialog();
+                }
+            });
+        }
+    }
+
+    public void onGoogleSignInClicked() {
+        Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient);
+        startActivityForResult(signInIntent, REQUEST_CODE_SIGN_IN);
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
+        if (requestCode == REQUEST_CODE_SIGN_IN) {
+            GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+            handleSignInResult(result);
+        }
+    }
+
+    @SuppressWarnings("ConstantConditions")
+    private void handleSignInResult(GoogleSignInResult result) {
+        Log.d(TAG, "handleSignInResult:" + result.isSuccess());
+        if (result.isSuccess()) {
+            // Signed in successfully, show authenticated UI.
+            GoogleSignInAccount acct = result.getSignInAccount();
+            // TODO get token and send to firebase
+            // acct.getIdToken();
+
+            findViewById(R.id.btn_google_sign_in).setVisibility(View.GONE);
+            findViewById(R.id.btn_new_post).setVisibility(View.VISIBLE);
+        } else {
+            findViewById(R.id.btn_google_sign_in).setVisibility(View.VISIBLE);
+            findViewById(R.id.btn_new_post).setVisibility(View.GONE);
+            // Signed out, show unauthenticated UI.
+        }
+    }
+
+    private void showProgressDialog() {
+        if (mProgressDialog == null) {
+            mProgressDialog = new ProgressDialog(this);
+            mProgressDialog.setMessage(getString(R.string.loading));
+            mProgressDialog.setIndeterminate(true);
+        }
+
+        mProgressDialog.show();
+    }
+
+    private void hideProgressDialog() {
+        if (mProgressDialog != null && mProgressDialog.isShowing()) {
+            mProgressDialog.hide();
         }
     }
 
