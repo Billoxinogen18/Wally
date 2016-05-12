c2266444cb1df0fb3e4c3251b790aa9a809c37a2
Viper <mkenc12@freeuni.edu.ge>
2016-05-12 11:33:11 +0400

    Add async save and delete to FirebaseContent

--- app/src/main/java/com/wally/wally/datacontroller/firebase/FirebaseDAL.java
+++ app/src/main/java/com/wally/wally/datacontroller/firebase/FirebaseDAL.java
@@ -6,13 +6,14 @@ import com.firebase.client.FirebaseError;
 import com.firebase.client.ValueEventListener;
 import com.wally.wally.datacontroller.Callback;
 import com.wally.wally.datacontroller.DataAccessLayer;
+import com.wally.wally.datacontroller.content.Content;
 
 import java.util.Collection;
 import java.util.HashSet;
 import java.util.Set;
 
 @Deprecated
-public class FirebaseDAL implements DataAccessLayer<FirebaseContent, FirebaseQuery> {
+public class FirebaseDAL implements DataAccessLayer<Content, FirebaseQuery> {
     private Firebase fb;
 
     public FirebaseDAL(Firebase inner) {
@@ -20,64 +21,36 @@ public class FirebaseDAL implements DataAccessLayer<FirebaseContent, FirebaseQue
     }
 
     @Override
-    public void save(final FirebaseContent c, final Callback<Boolean> statusCallback) {
-        fb.push().setValue(c, new Firebase.CompletionListener() {
-            @Override
-            public void onComplete(FirebaseError firebaseError, Firebase firebase) {
-                if (firebaseError == null) {
-                    c.setId(firebase.getKey());
-                    statusCallback.call(true, null);
-                } else {
-                    statusCallback.call(false, firebaseError.toException());
-                }
-            }
-        });
+    public void save(final Content c, final Callback<Boolean> statusCallback) {
+        new FirebaseContent(c).save(fb, statusCallback);
     }
 
     @Override
-    public void save(FirebaseContent c) {
-        if (c.getId() == null) {
-            Firebase reference = fb.push();
-            reference.setValue(c);
-            c.setId(reference.getKey());
-        } else {
-            fb.child(c.getId()).setValue(c);
-        }
+    public void save(Content c) {
+        new FirebaseContent(c).save(fb);
     }
 
     @Override
-    public void delete(FirebaseContent c, final Callback<Boolean> statusCallback) {
-        fb.child(c.getId()).removeValue(new Firebase.CompletionListener() {
-            @Override
-            public void onComplete(FirebaseError firebaseError, Firebase firebase) {
-                if (firebaseError == null) {
-                    statusCallback.call(true, null);
-                } else {
-                    statusCallback.call(false, firebaseError.toException());
-                }
-            }
-        });
-
+    public void delete(Content c, final Callback<Boolean> statusCallback) {
+        new FirebaseContent(c).delete(fb, statusCallback);
     }
 
     @Override
-    public void delete(FirebaseContent c) {
-        fb.child(c.getId()).removeValue();
-    }
+    public void delete(Content c) { new FirebaseContent(c).delete(fb); }
 
     @Override
     public void fetch(final FirebaseQuery query,
-                      final Callback<Collection<FirebaseContent>> resultCallback) {
+                      final Callback<Collection<Content>> resultCallback) {
 
         query.getTarget(fb).addListenerForSingleValueEvent(new ValueEventListener() {
 
             @Override
             public void onDataChange(DataSnapshot snapshot) {
-                Set<FirebaseContent> result = new HashSet<>();
+                Set<Content> result = new HashSet<>();
                 for (DataSnapshot contentSnapshot: snapshot.getChildren()) {
-                    FirebaseContent content = contentSnapshot.getValue(FirebaseContent.class);
-                    content.setId(contentSnapshot.getKey());
-                    result.add(content);
+                    FirebaseContent fbcontent = contentSnapshot.getValue(FirebaseContent.class);
+                    fbcontent.setId(contentSnapshot.getKey());
+                    result.add(fbcontent.toContent());
                 }
                 resultCallback.call(result, null);
             }
