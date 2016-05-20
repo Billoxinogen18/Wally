5dcb05783dbe04afa40b09c82a99e3c879e2014e
Ioane Sharvadze <ioane.sharvadze@gmail.com>
2016-05-20 23:49:25 +0400

    Add profile content grid

--- app/src/main/java/com/wally/wally/activities/ProfileActivity.java
+++ app/src/main/java/com/wally/wally/activities/ProfileActivity.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.design.widget.AppBarLayout;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.PopupMenu;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.StaggeredGridLayoutManager;
 import android.support.v7.widget.Toolbar;
@@ -14,6 +15,8 @@ import android.text.TextUtils;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.animation.AlphaAnimation;
@@ -24,16 +27,19 @@ import android.widget.TextView;
 import com.bumptech.glide.Glide;
 import com.wally.wally.App;
 import com.wally.wally.R;
+import com.wally.wally.Utils;
 import com.wally.wally.components.CircleTransform;
 import com.wally.wally.datacontroller.callbacks.FetchResultCallback;
 import com.wally.wally.datacontroller.content.Content;
+import com.wally.wally.fragments.NewContentDialogFragment;
+import com.wally.wally.fragments.PreviewContentDialogFragment;
 import com.wally.wally.userManager.SocialUser;
 
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
-public class ProfileActivity extends AppCompatActivity implements AppBarLayout.OnOffsetChangedListener, FetchResultCallback {
+public class ProfileActivity extends AppCompatActivity implements AppBarLayout.OnOffsetChangedListener, FetchResultCallback, NewContentDialogFragment.NewContentDialogListener {
 
     private static final String TAG = ProfileActivity.class.getSimpleName();
 
@@ -168,15 +174,21 @@ public class ProfileActivity extends AppCompatActivity implements AppBarLayout.O
         app.getDataController().fetchByAuthor(app.getUser().getBaseUser(), this);
 
         RecyclerView recyclerView = (RecyclerView) findViewById(R.id.recyclerview);
-        recyclerView.setLayoutManager(new StaggeredGridLayoutManager(3, StaggeredGridLayoutManager.VERTICAL));
+        recyclerView.setLayoutManager(new StaggeredGridLayoutManager(
+                getGridColumnCount(), StaggeredGridLayoutManager.VERTICAL));
         recyclerView.setAdapter(new ContentAdapter());
 
         mContentAdapter = new ContentAdapter();
         recyclerView.setAdapter(mContentAdapter);
-
         // TODO add loading, tryAgain, empty screen
     }
 
+    /**
+     * @return optimal quantity based on rotation and size
+     */
+    private int getGridColumnCount() {
+        return (int) (Utils.getScreenWidthDpi(this) / 170);
+    }
 
     /**
      * Called when data is loaded
@@ -185,32 +197,55 @@ public class ProfileActivity extends AppCompatActivity implements AppBarLayout.O
      */
     @Override
     public void onResult(Collection<Content> result) {
+        Log.d(TAG, "Loaded content size = [" + result.size() + "]");
         ArrayList<Content> data = new ArrayList<>(result.size());
         data.addAll(result);
+        // TODO delete dumb data
+        data.add(new Content().withId("0").withNote("Hi there my name is...").withTitle("Sample note"));
+        data.add(new Content().withId("5").withNote(getString(R.string.large_text)).withTitle("თქვენ შიგ ხო არ გაქვთ რა ლიმიტი").withImageUri("http://i.imgur.com/RRUe0Mo.png"));
+        data.add(new Content().withId("6").withNote(getString(R.string.large_text)).withTitle("Sample note Title here"));
+        data.add(new Content().withId("7").withNote("Hi there my name is John").withTitle("Sample note"));
+        data.add(new Content().withId("8").withNote("Hi there my name is... I'm programmer here :S"));
+        data.add(new Content().withId("9").withTitle("Sample note Only title"));
+        data.add(new Content().withId("10").withTitle("Sample note").withImageUri("http://www.keenthemes.com/preview/metronic/theme/assets/global/plugins/jcrop/demos/demo_files/image1.jpg"));
 
         mContentAdapter.setData(data);
-        result.size();
-        Log.d(TAG, "onResult() called with: " + "result = [" + result.size() + "]");
     }
 
     @Override
     public void onError(Exception e) {
         // TODO get exception and show no internet UI or something.
+        Log.e(TAG, "onError: " + e);
+    }
+
+    private void onDeleteContent(Content content) {
+        mContentAdapter.removeItem(content);
+        // TODO call db
+    }
+
+    private void onEditContent(Content content) {
+        NewContentDialogFragment.newInstance(content).show(getSupportFragmentManager(), NewContentDialogFragment.TAG);
+    }
+
+    @Override
+    public void onContentCreated(Content content, boolean isEditMode) {
+        Log.d(TAG, "onContentCreated() called with: " + "content = [" + content + "], isEditMode = [" + isEditMode + "]");
+        mContentAdapter.updateItem(content);
+        // TODO call db
+    }
+
+    private void onContentClicked(Content content, View view) {
+        PreviewContentDialogFragment pd = PreviewContentDialogFragment.newInstance(content);
+        pd.show(getSupportFragmentManager(), PreviewContentDialogFragment.TAG);
     }
 
     private class ContentAdapter extends RecyclerView.Adapter<ContentAdapter.ViewHolder> {
 
         private List<Content> mData;
 
-        public void setData(List<Content> data) {
-            mData = data;
-            // this thing adds insert animation and updates data
-            notifyItemRangeInserted(0, getItemCount());
-        }
-
         @Override
         public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-            @SuppressLint("InflateParams") View v = LayoutInflater.from(getBaseContext())
+            @SuppressLint("InflateParams") View v = LayoutInflater.from(parent.getContext())
                     .inflate(R.layout.layout_content_grid, null);
             return new ViewHolder(v);
         }
@@ -233,12 +268,34 @@ public class ProfileActivity extends AppCompatActivity implements AppBarLayout.O
             vh.title.setVisibility(TextUtils.isEmpty(c.getTitle()) ? View.GONE : View.VISIBLE);
 
             vh.note.setText(c.getNote());
-            vh.note.setVisibility(TextUtils.isEmpty(c.getTitle()) ? View.GONE : View.VISIBLE);
+            vh.note.setVisibility(TextUtils.isEmpty(c.getNote()) ? View.GONE : View.VISIBLE);
         }
 
         @Override
         public int getItemCount() {
             return mData == null ? 0 : mData.size();
+        }
+
+        public void setData(List<Content> data) {
+            mData = data;
+            // this thing adds insert animation and updates data
+            notifyItemRangeInserted(0, getItemCount());
+        }
+
+        public int getItemPosition(Content content) {
+            return mData.indexOf(content);
+        }
+
+        public void removeItem(Content content) {
+            int pos = getItemPosition(content);
+            mData.remove(pos);
+            notifyItemRemoved(pos);
+        }
+
+        public void updateItem(Content content) {
+            int pos = getItemPosition(content);
+            mData.set(pos, content);
+            notifyItemChanged(pos);
         }
 
         public class ViewHolder extends RecyclerView.ViewHolder {
@@ -247,11 +304,47 @@ public class ProfileActivity extends AppCompatActivity implements AppBarLayout.O
             public TextView title;
             public ImageView image;
 
-            public ViewHolder(View itemView) {
-                super(itemView);
-                note = (TextView) findViewById(R.id.tv_note);
-                title = (TextView) findViewById(R.id.tv_title);
-                image = (ImageView) findViewById(R.id.image_view);
+            public ViewHolder(View v) {
+                super(v);
+                note = (TextView) v.findViewById(R.id.tv_note);
+                title = (TextView) v.findViewById(R.id.tv_title);
+                image = (ImageView) v.findViewById(R.id.image_view);
+                v.findViewById(R.id.btn_more_settings).setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        showPopup(v);
+                    }
+                });
+
+                v.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Content content = mData.get(getAdapterPosition());
+                        onContentClicked(content, v);
+                    }
+                });
+            }
+
+            public void showPopup(View v) {
+                PopupMenu popup = new PopupMenu(v.getContext(), v);
+                MenuInflater inflater = popup.getMenuInflater();
+                inflater.inflate(R.menu.grid_content_actions, popup.getMenu());
+                popup.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+                    @Override
+                    public boolean onMenuItemClick(MenuItem item) {
+                        Content content = mData.get(getAdapterPosition());
+                        switch (item.getItemId()) {
+                            case R.id.delete:
+                                onDeleteContent(content);
+                                break;
+                            case R.id.edit:
+                                onEditContent(content);
+                                break;
+                        }
+                        return false;
+                    }
+                });
+                popup.show();
             }
         }
     }
