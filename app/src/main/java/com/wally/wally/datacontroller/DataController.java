742ccda03f4baf7a847fd53783c220952727ad24
Viper <mkenc12@freeuni.edu.ge>
2016-05-26 16:30:22 +0400

    Implement image upload

--- app/src/main/java/com/wally/wally/datacontroller/DataController.java
+++ app/src/main/java/com/wally/wally/datacontroller/DataController.java
@@ -1,10 +1,19 @@
 package com.wally.wally.datacontroller;
 
+import android.net.Uri;
+import android.support.annotation.NonNull;
+import android.util.Log;
+
 import com.google.android.gms.maps.model.LatLngBounds;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 import com.wally.wally.datacontroller.queries.*;
 import com.wally.wally.datacontroller.callbacks.*;
 import com.wally.wally.datacontroller.content.Content;
@@ -12,32 +21,67 @@ import com.wally.wally.datacontroller.content.FirebaseContent;
 import com.wally.wally.datacontroller.user.Id;
 import com.wally.wally.datacontroller.user.User;
 
+import java.io.File;
+import java.util.UUID;
+
 public class DataController {
     public static final String TAG = DataController.class.getSimpleName();
 
-    private static final String ROOT_NODE = "Develop";
+    private static final String DATABASE_ROOT = "Develop";
+    private static final String STORAGE_ROOT = "Develop";
     private static final String USERS_NODE = "Users";
     private static final String CONTENTS_NODE = "Contents";
 
     private static DataController instance;
 
+    private StorageReference storage;
     private DatabaseReference users, contents;
 
-    private DataController(DatabaseReference firebase) {
-        users = firebase.child(USERS_NODE);
-        contents = firebase.child(CONTENTS_NODE);
+    private DataController(DatabaseReference database, StorageReference storage) {
+        this.storage = storage;
+        users = database.child(USERS_NODE);
+        contents = database.child(CONTENTS_NODE);
     }
 
     public static DataController create() {
         if (instance == null) {
-            DatabaseReference ref = FirebaseDatabase.getInstance().getReference();
-            instance = new DataController(ref.child(ROOT_NODE));
+            DatabaseReference database = FirebaseDatabase.getInstance().getReference();
+            StorageReference storage = FirebaseStorage.getInstance()
+                    .getReferenceFromUrl("gs://wally-virtual-notes.appspot.com/");
+            instance = new DataController(
+                    database.child(DATABASE_ROOT),
+                    storage.child(STORAGE_ROOT));
         }
         return instance;
     }
 
-    public void save(Content c) {
-        new FirebaseContent(c).save(contents);
+    public void save(final Content c) {
+        String imgUriString = c.getImageUri().substring(7);
+        Uri imgUri = Uri.fromFile(new File(imgUriString));
+        String imageName = UUID.randomUUID().toString();
+        UploadTask imageUploadTask = storage.child(imageName).putFile(imgUri);
+        Log.d(TAG, imgUriString);
+
+        imageUploadTask.addOnSuccessListener(
+                new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                    @Override
+                    public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                        Uri downloadUri = taskSnapshot.getDownloadUrl();
+                        if (downloadUri == null) return; // TODO retry upload?
+                        Log.d(TAG, downloadUri.toString());
+                        c.withImageUri(downloadUri.toString());
+                        new FirebaseContent(c).save(contents);
+                        
+                    }
+                }
+        ).addOnFailureListener(
+                new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        // TODO retry upload?
+                    }
+                }
+        );
     }
 
     public void delete(Content c) {
