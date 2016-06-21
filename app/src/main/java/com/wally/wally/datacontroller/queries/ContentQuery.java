c146e2eea5fedb89d765e82a4a1afd293e89dd28
Viper <mkenc12@freeuni.edu.ge>
2016-06-21 23:10:42 +0400

    Add ContentQuery

---
+++ app/src/main/java/com/wally/wally/datacontroller/queries/ContentQuery.java
@@ -0,0 +1,54 @@
+package com.wally.wally.datacontroller.queries;
+
+import com.google.firebase.database.DatabaseReference;
+import com.wally.wally.datacontroller.Predicate;
+import com.wally.wally.datacontroller.callbacks.Callback;
+import com.wally.wally.datacontroller.callbacks.FetchResultCallback;
+import com.wally.wally.datacontroller.content.Content;
+import com.wally.wally.datacontroller.content.FirebaseContent;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+public class ContentQuery {
+    private final DatabaseReference ref;
+    private final FirebaseQuery query;
+    private final Predicate<Content> predicate;
+
+
+    public ContentQuery(FirebaseQuery query,
+                        DatabaseReference ref,
+                        Predicate<Content> predicate) {
+        this.ref = ref;
+        this.query = query;
+        this.predicate = predicate;
+    }
+
+
+    public ContentQuery(FirebaseQuery query, DatabaseReference ref) {
+        this(query, ref, null);
+    }
+
+
+    public void fetch(final FetchResultCallback callback) {
+        query.fetch(ref, new Callback<Collection<FirebaseContent>>() {
+            @Override
+            public void onResult(Collection<FirebaseContent> fetched) {
+                List<Content> result = new ArrayList<>();
+                for (FirebaseContent c : fetched) {
+                    Content content = c.toContent();
+                    if (predicate.test(content)) {
+                        result.add(content);
+                    }
+                }
+                callback.onResult(result);
+            }
+
+            @Override
+            public void onError(Exception e) {
+                callback.onError(e);
+            }
+        });
+    }
+}
