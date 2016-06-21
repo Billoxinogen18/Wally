1e899ec51b4d80566a6f78c4fc2ae1fc199fc729
Viper <mkenc12@freeuni.edu.ge>
2016-06-21 23:10:01 +0400

    Add Predicate<T>

---
+++ app/src/main/java/com/wally/wally/datacontroller/Predicate.java
@@ -0,0 +1,34 @@
+package com.wally.wally.datacontroller;
+
+
+public abstract class Predicate<T> {
+
+    public abstract boolean test(T target);
+
+    public Predicate<T> not() {
+        return new Predicate<T>() {
+            @Override
+            public boolean test(T target) {
+                return !Predicate.this.test(target);
+            }
+        };
+    }
+
+    public Predicate<T> and(final Predicate<T> other) {
+        return new Predicate<T>() {
+            @Override
+            public boolean test(T target) {
+                return Predicate.this.test(target) && other.test(target);
+            }
+        };
+    }
+
+    public Predicate<T> or(final Predicate other) {
+        return new Predicate<T>() {
+            @Override
+            public boolean test(T target) {
+                return Predicate.this.test(target) || other.test(target);
+            }
+        };
+    }
+}
