abc8c7dbaef7596e63d317fdda7ac6912322a3ee
Viper <mkenc12@freeuni.edu.ge>
2016-07-13 15:40:51 +0400

    Implement AdfManager

--- app/src/main/java/com/wally/wally/adfCreator/AdfInfo.java
+++ app/src/main/java/com/wally/wally/adfCreator/AdfInfo.java
@@ -1,21 +1,39 @@
 package com.wally.wally.adfCreator;
 
 
-/**
- * Created by shota on 7/11/16.
- */
 public class AdfInfo {
+    private String path;
+    private String uuid;
 
-    public boolean isImported(){
-        return true;
+    public AdfInfo() {
+
+    }
+
+    public AdfInfo(AdfInfo other) {
+        this.path = other.path;
+        this.uuid = other.uuid;
+    }
+
+    public AdfInfo withUuid(String uuid) {
+        this.uuid = uuid;
+        return this;
+    }
+
+    public AdfInfo withPath(String path) {
+        this.path = path;
+        return this;
     }
 
     public String getPath(){
-        return "";
+        return path;
     }
 
     public String getUuid(){
-        return "";
+        return uuid;
+    }
+
+    public boolean isImported(){
+        return true;
     }
 
 }
--- app/src/main/java/com/wally/wally/adfCreator/AdfManager.java
+++ app/src/main/java/com/wally/wally/adfCreator/AdfManager.java
@@ -1,21 +1,70 @@
 package com.wally.wally.adfCreator;
 
+import com.wally.wally.Utils;
+import com.wally.wally.datacontroller.adf.ADFService;
+import com.wally.wally.datacontroller.callbacks.Callback;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.concurrent.CountDownLatch;
+
 /**
  * Lazy retrieval of ADF files from the cloud.
  * Created by shota on 7/11/16.
  */
 public class AdfManager {
+    private CountDownLatch latch;
+    private List<String> uuids;
+    private ADFService adfService;
+    private AdfInfo current;
+
+    public AdfManager(ADFService adfService) {
+        this.adfService = adfService;
+        this.uuids = new LinkedList<>();
+    }
+
+    public AdfManager withUuids(List<String> uuids) {
+        this.uuids.addAll(uuids);
+        downloadNext();
+        return this;
+    }
+
     public boolean hasAdf(){
-        return true;
+        return uuids.size() > 0;
     }
 
     public boolean isAdfReady(){
-        return true;
+        return latch.getCount() == 0;
     }
 
     public AdfInfo getAdf(){
-        return new AdfInfo();
+        try {
+            latch.await();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        AdfInfo result = new AdfInfo(current);
+        downloadNext();
+        return result;
     }
 
+    private void downloadNext() {
+        latch = new CountDownLatch(1);
+        final String uuid = uuids.get(0);
+        final String path = Utils.getAdfFilePath(uuid);
+        adfService.download(path, uuid, new Callback<Void>() {
+            @Override
+            public void onResult(Void result) {
+                current = new AdfInfo().withUuid(uuid).withPath(path);
+                latch.countDown();
+            }
 
+            @Override
+            public void onError(Exception e) {
+                // TODO leave empty for now
+                // Warning: if we somehow get here
+                // No more adf will be downloaded anymore
+            }
+        });
+    }
 }
