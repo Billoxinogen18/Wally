8c81b0110f44cc2b5b03e3fe72c05ceeddf30b1a
Viper <mkenc12@freeuni.edu.ge>
2016-07-25 15:32:18 +0400

    Implement stub ValidPoserListener in LearningEvaluator

--- app/src/main/java/com/wally/wally/tango/LearningEvaluator.java
+++ app/src/main/java/com/wally/wally/tango/LearningEvaluator.java
@@ -1,20 +1,31 @@
 package com.wally.wally.tango;
 
 
-import com.wally.wally.datacontroller.callbacks.Callback;
+import android.util.Log;
 
-public class LearningEvaluator {
+import com.google.atap.tangoservice.TangoPoseData;
 
+public class LearningEvaluator implements TangoUpdater.ValidPoseListener {
+    public static final String TAG = LearningEvaluator.class.getSimpleName();
+    public static final int TIMEOUT_S = 20;
     public void addCallback(final LearningEvaluatorListener listener) {
+
         new Thread(new Runnable() {
             @Override
             public void run() {
                 try {
-                    Thread.sleep(20000);
-                } catch (InterruptedException e) {}
+                    Thread.sleep(TIMEOUT_S * 1000);
+                } catch (InterruptedException e) {
+                    listener.onLearningFailed();
+                }
                 listener.onLearningFinish();
             }
         }).start();
+    }
+
+    @Override
+    public void onValidPose(TangoPoseData pose) {
+        Log.d(TAG, "" + (pose.statusCode == TangoPoseData.POSE_VALID));
     }
 
     interface LearningEvaluatorListener{
