f7dd9bb6539ea151b8beec5e77b2ccc270d25060
Viper <mkenc12@freeuni.edu.ge>
2016-08-04 16:55:17 +0400

    Add Tip class
    close: #29

---
+++ app/src/main/java/com/wally/wally/tip/Tip.java
@@ -0,0 +1,27 @@
+package com.wally.wally.tip;
+
+
+public class Tip {
+    private String title;
+    private String message;
+
+    public Tip() {}
+
+    public Tip withTitle(String title) {
+        this.title = title;
+        return this;
+    }
+
+    public Tip withMessage(String message) {
+        this.message = message;
+        return this;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+}
