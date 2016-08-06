ff27b389cbe35d3da027345af60f9590a70d1648
Viper <mkenc12@freeuni.edu.ge>
2016-08-06 14:32:46 +0400

    Add stub LocalTipService

---
+++ app/src/main/java/com/wally/wally/tip/LocalTipService.java
@@ -0,0 +1,14 @@
+package com.wally.wally.tip;
+
+public class LocalTipService implements TipService {
+
+    @Override
+    public Tip getRandom() {
+        return null;
+    }
+
+    @Override
+    public Tip getRandom(String tag) {
+        return null;
+    }
+}
--- app/src/main/java/com/wally/wally/tip/TipService.java
+++ app/src/main/java/com/wally/wally/tip/TipService.java
@@ -1,6 +1,6 @@
 package com.wally.wally.tip;
 
-public interface TipManager {
+public interface TipService {
     class Tag {
         public static final String LEARNING = "learning";
         public static final String LOCALIZATION = "localization";
