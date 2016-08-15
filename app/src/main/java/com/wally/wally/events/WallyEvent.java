e7eb14e1f85a73c219365d85c44334094d0f0e7f
Viper <mkenc12@freeuni.edu.ge>
2016-08-15 14:31:46 +0400

    Update WallyEvent

--- app/src/main/java/com/wally/wally/events/WallyEvent.java
+++ app/src/main/java/com/wally/wally/events/WallyEvent.java
@@ -2,30 +2,40 @@ package com.wally.wally.events;
 
 public class WallyEvent {
     public static final String
-    TANGO_READY ="onTangoReady";
-    
-    public static final String
-    LEARNING_START ="onLearningStart";
+    TANGO_READY = "onTangoReady";
 
     public static final String
-    TANGO_OUT_OF_DATE ="onTangoOutOfDate";
+    TANGO_OUT_OF_DATE = "onTangoOutOfDate";
 
     public static final String
-    LEARNING_FINISH ="onLearningFinish";
+    LEARNING_START = "onLearningStart";
 
     public static final String
-    LOCALIZATION_START ="onLocalizationStart";
+    LEARNING_FINISH = "onLearningFinish";
 
     public static final String
-    LOCALIZATION_START_AFTER_LEARNING ="onLocalizationStartAfterLearning";
+    LOCALIZATION_START = "onLocalizationStart";
 
     public static final String
-    LOCALIZATION_FINISH_AFTER_LEARNING ="onLocalizationFinishAfterLearning";
+    LOCALIZATION_START_AFTER_LEARNING = "onLocalizationStartAfterLearning";
 
     public static final String
-    LOCALIZATION_FINISH_AFTER_SAVED_ADF ="onLocalizationFinishAfterSavedAdf";
+    LOCALIZATION_FINISH_AFTER_LEARNING = "onLocalizationFinishAfterLearning";
+
+    public static final String
+    LOCALIZATION_FINISH_AFTER_SAVED_ADF = "onLocalizationFinishAfterSavedAdf";
+
+    private final String id;
+
+    private WallyEvent(String id) {
+        this.id = id;
+    }
+
+    public String getId() {
+        return id;
+    }
 
     public static WallyEvent createEventWithId(String id) {
-        return null;
+        return new WallyEvent(id);
     }
 }
