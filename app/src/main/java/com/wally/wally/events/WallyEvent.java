07fba507a1c3bce3c19ba0609e01f83efd02109e
Viper <mkenc12@freeuni.edu.ge>
2016-08-15 13:03:29 +0400

    Add stub WallyEvent and WallyEventListener

---
+++ app/src/main/java/com/wally/wally/events/WallyEvent.java
@@ -0,0 +1,5 @@
+package com.wally.wally.events;
+
+public class WallyEvent {
+    public static final String EVENT_1 = "event1";
+}
---
+++ app/src/main/java/com/wally/wally/events/WallyEventListener.java
@@ -0,0 +1,5 @@
+package com.wally.wally.events;
+
+public interface WallyEventListener {
+    void onWallyEvent(WallyEvent event);
+}
