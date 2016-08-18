76e807146ec096483e14a937befc4667e1aa3453
Gio <ggogi12@freeuni.edu.ge>
2016-08-18 17:28:15 +0400

    Implement Jquery.cycle plugin

---
+++ js/jquery.cycle.js
@@ -0,0 +1,21 @@
+(function ($) {
+
+    $.fn.cycle = function (selector) {
+        console.log("bla");
+        var $container = $(this);
+
+        function cycleImages(){
+            var $active = $container.find('.active') || $container.find(selector);
+            var $next = ($active.next(selector).length > 0) ? $active.next(selector) : $container.find(selector + ':first');
+            $next.css('z-index',2);//move the next image up the pile
+            $active.fadeOut(1500,function(){//fade out the top image
+                $active.css('z-index',1).show().removeClass('active');//reset the z-index and unhide the image
+                $next.css('z-index',3).addClass('active');//make the next image the top one
+            });
+        }
+
+        setInterval(cycleImages, 2000);
+
+        return this;
+    };
+})(jQuery);
