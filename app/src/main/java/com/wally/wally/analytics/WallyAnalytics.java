6c6d29f01857a2caf28d3fa84e59abd477b48546
Viper <mkenc12@freeuni.edu.ge>
2016-08-18 14:12:17 +0400

    Clean up WallyAnalytics

--- app/src/main/java/com/wally/wally/analytics/WallyAnalytics.java
+++ app/src/main/java/com/wally/wally/analytics/WallyAnalytics.java
@@ -4,103 +4,52 @@ import android.content.Context;
 import android.os.Bundle;
 
 import com.google.firebase.analytics.FirebaseAnalytics;
-import com.wally.wally.events.EventListener;
+import com.wally.wally.events.WallyEvent;
+import com.wally.wally.events.WallyEventListener;
 
-public class WallyAnalytics implements EventListener {
+public class WallyAnalytics implements WallyEventListener {
     public static final String TAG = WallyAnalytics.class.getSimpleName();
     private static WallyAnalytics instance;
     private final FirebaseAnalytics analytics;
 
     public WallyAnalytics(FirebaseAnalytics analytics) {
         this.analytics = analytics;
-
-    }
-
-    public void onLocalizeOnDownloadedAdf(boolean status) {
-        Bundle bundle = new Bundle();
-        bundle.putInt("Count", 1);
-        if (status) {
-            analytics.logEvent("ADF_dnl_localize_success", bundle);
-        } else {
-            analytics.logEvent("ADF_dnl_localize_fail", bundle);
-        }
-    }
-
-    public void onLocalizeAfterResume(boolean status) {
-        Bundle bundle = new Bundle();
-        bundle.putInt("Count", 1);
-        if (status) {
-            analytics.logEvent("ADF_resume_localize_success", bundle);
-        } else {
-            analytics.logEvent("ADF_resume_localize_fail", bundle);
-        }
-    }
-
-    public void logAdfNumberBeforeLocalization(int n) {
-        Bundle bundle = new Bundle();
-        bundle.putInt("Number", n);
-        analytics.logEvent("ADF_count_before_localization", bundle);
-    }
-
-    public void logAdfNumberBeforeLearning(int n) {
-        Bundle bundle = new Bundle();
-        bundle.putInt("Number", n);
-        analytics.logEvent("ADF_count_before_learning", bundle);
     }
 
     public void onButtonClick(String buttonId) {
-        Bundle bundle = new Bundle();
-        bundle.putInt("Count", 1);
+        Bundle bundle = getDefaultBundle();
         bundle.putString("Button_Id", buttonId);
         analytics.logEvent("Button_Click", bundle);
     }
 
+    @SuppressWarnings("unused")
     public void onContentCreate() {
-        Bundle bundle = new Bundle();
-        bundle.putInt("Count", 1);
-        analytics.logEvent("Content_Create", bundle);
-    }
-
-    public void logContentType(String type) {
-        Bundle bundle = new Bundle();
-        bundle.putInt("Count", 1);
-        bundle.putString("Type", type);
-        analytics.logEvent("Content_Type", bundle);
+        analytics.logEvent("Content_Create", getDefaultBundle());
     }
 
     public void onContentDelete() {
-        Bundle bundle = new Bundle();
-        bundle.putInt("Count", 1);
-        analytics.logEvent("Content_Delete", bundle);
+        analytics.logEvent("Content_Delete", getDefaultBundle());
     }
 
+    @SuppressWarnings("unused")
     public void onContentEdit() {
-        Bundle bundle = new Bundle();
-        bundle.putInt("Count", 1);
-        analytics.logEvent("Content_Edit", bundle);
+        analytics.logEvent("Content_Edit", getDefaultBundle());
     }
 
+    @SuppressWarnings("unused")
     public void onContentSelect() {
-        Bundle bundle = new Bundle();
-        bundle.putInt("Count", 1);
-        analytics.logEvent("Content_Select", bundle);
+        analytics.logEvent("Content_Select", getDefaultBundle());
     }
 
-    public void logLocalizationTimeForAdf(String uuid, long timeMs) {
-        Bundle bundle = new Bundle();
-        bundle.putString("uuid", uuid);
-        bundle.putLong("time", timeMs);
-        analytics.logEvent("ADF_TIME_ON_LOCALIZATION", bundle);
-    }
-
-    public void onLocalizeOnNewAdf(boolean status) {
+    private Bundle getDefaultBundle() {
         Bundle bundle = new Bundle();
         bundle.putInt("Count", 1);
-        if (status) {
-            analytics.logEvent("ADF_new_localize_success", bundle);
-        } else {
-            analytics.logEvent("ADF_new_localize_fail", bundle);
-        }
+        return  bundle;
+    }
+
+    @Override
+    public void onWallyEvent(WallyEvent event) {
+        analytics.logEvent(event.getId(), getDefaultBundle());
     }
 
     public static WallyAnalytics getInstance(Context context) {
@@ -109,51 +58,5 @@ public class WallyAnalytics implements EventListener {
             instance = new WallyAnalytics(analytics);
         }
         return instance;
-    }
-
-    @Override
-    public void onTangoReady() {
-
-    }
-
-    @Override
-    public void onLearningStart() {
-
-    }
-
-    @Override
-    public void onTangoOutOfDate() {
-
-    }
-
-    @Override
-    public void onLearningFinish() {
-        analytics.logEvent("ADF_created", getDefaultBundle());
-    }
-
-    @Override
-    public void onLocalizationStart() {
-
-    }
-
-    @Override
-    public void onLocalizationStartAfterLearning() {
-
-    }
-
-    @Override
-    public void onLocalizationFinishAfterLearning() {
-        analytics.logEvent("ADF_new_localize_success", getDefaultBundle());
-    }
-
-    @Override
-    public void onLocalizationFinishAfterSavedAdf() {
-
-    }
-
-    private Bundle getDefaultBundle() {
-        Bundle bundle = new Bundle();
-        bundle.putInt("Count", 1);
-        return  bundle;
     }
 }
