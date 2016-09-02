db1c74c8a357417070d91c845e37ea5127fe1f7e
Ioane Sharvadze <ioane.sharvadze@gmail.com>
2016-09-02 16:27:07 +0300

    Implement case insensitive answer check for puzzle

--- app/src/main/java/com/wally/wally/datacontroller/content/Puzzle.java
+++ app/src/main/java/com/wally/wally/datacontroller/content/Puzzle.java
@@ -4,9 +4,8 @@ import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
-public class Puzzle implements Serializable{
+public class Puzzle implements Serializable {
     // Something serializable here
     private HashSet<String> answers;
 
@@ -24,6 +23,12 @@ public class Puzzle implements Serializable{
     }
 
     public boolean checkAnswer(String answer) {
-        return this.answers.contains(answer.toLowerCase());
+        answer = answer.toLowerCase();
+        for (String s : this.answers) {
+            if (s.toLowerCase().equals(answer)) {
+                return true;
+            }
+        }
+        return false;
     }
 }
