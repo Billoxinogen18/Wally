76922e300b0ba2f2477ca89b8f188c31bfec7c6b
Viper <mkenc12@freeuni.edu.ge>
2016-09-16 19:18:57 +0300

    Refactor inheritance of tangoForLearnedAdf

--- app/src/main/java/com/wally/wally/tango/states/TangoForLearnedAdf.java
+++ app/src/main/java/com/wally/wally/tango/states/TangoForLearnedAdf.java
@@ -1,20 +1,25 @@
 package com.wally.wally.tango.states;
 
-import android.util.Log;
-
 import com.projecttango.tangosupport.TangoPointCloudManager;
+import com.wally.wally.adf.AdfInfo;
 import com.wally.wally.events.WallyEvent;
 import com.wally.wally.renderer.WallyRenderer;
 import com.wally.wally.tango.TangoFactory;
 import com.wally.wally.tango.TangoUpdater;
+import com.wally.wally.tango.WatchDog;
 
 /**
  * Created by shota on 8/9/16.
  * Manages Tango for learned Adf
  */
-public class TangoForLearnedAdf extends TangoForAdf {
-    private static final String TAG = TangoForLearnedAdf.class.getSimpleName();
+public class TangoForLearnedAdf extends TangoState {
+    private static final WallyEvent LOC_FINISH_EVENT =
+            WallyEvent.createEventWithId(WallyEvent.LOCALIZATION_FINISH_AFTER_LEARNING);
+    private static final WallyEvent LOC_START_EVENT =
+            WallyEvent.createEventWithId(WallyEvent.LOCALIZATION_START_AFTER_LEARNING);
 
+    private AdfInfo mAdfInfo;
+    private WatchDog mLocalizationWatchdog;
 
     public TangoForLearnedAdf(TangoUpdater tangoUpdater,
                               TangoFactory tangoFactory,
@@ -22,32 +27,55 @@ public class TangoForLearnedAdf extends TangoForAdf {
                               TangoPointCloudManager pointCloudManager) {
 
         super(tangoUpdater, tangoFactory, wallyRenderer, pointCloudManager);
+        mLocalizationWatchdog = new WatchDog() {
+            @Override
+            protected void onTimeout() {
+                // TODO delete bad ADF
+                mFailStateConnector.toNextState();
+            }
+
+            @Override
+            protected boolean conditionFailed() {
+                return !mIsLocalized;
+            }
+        };
+    }
+
+    public TangoForLearnedAdf withAdf(AdfInfo adf){
+        mAdfInfo = adf;
+        return this;
+    }
+
+    public TangoForLearnedAdf withLocalizationTimeout(long timeout){
+        mLocalizationWatchdog = mLocalizationWatchdog.withTimeout(timeout);
+        return this;
     }
 
     @Override
     protected void pauseHook() {
-        mLocalizationWatchdog.interrupt();
+        mLocalizationWatchdog.disarm();
     }
 
     @Override
     protected void resumeHook() {
-        Log.d(TAG, "Localize With Learned adf = [" + mAdfInfo + "]. Thread = " + Thread.currentThread());
         mTango = mTangoFactory.getTangoWithUuid(getTangoInitializer(), mAdfInfo.getUuid());
-        Log.d(TAG, "resume() mTango = " + mTango + " isTangoConnected " + isConnected());
-        startLocalizationWatchDog();
-        fireLocalizationStartAfterLearning();
+        mLocalizationWatchdog.arm();
+        fireEvent(LOC_START_EVENT);
     }
 
     @Override
-    protected void fireLocalizationFinish() {
-        fireLocalizationFinishAfterLearning();
+    public void onLocalization(boolean localization) {
+        super.onLocalization(localization);
+        if (localization){
+            mLocalizationWatchdog.disarm();
+            fireEvent(LOC_FINISH_EVENT);
+            mSuccessStateConnector.toNextState();
+        }
     }
 
-    private void fireLocalizationStartAfterLearning() {
-        fireEvent(WallyEvent.createEventWithId(WallyEvent.LOCALIZATION_START_AFTER_LEARNING));
+    @Override
+    public AdfInfo getAdf() {
+        return mAdfInfo;
     }
 
-    private void fireLocalizationFinishAfterLearning() {
-        fireEvent(WallyEvent.createEventWithId(WallyEvent.LOCALIZATION_FINISH_AFTER_LEARNING));
-    }
 }
