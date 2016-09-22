28383eb4cdbda868f3ff134e72ea5ae4f0e839e9
Ioane Sharvadze <ioane.sharvadze@gmail.com>
2016-09-22 17:08:23 +0300

    Fix editNote bug

--- app/src/main/java/com/wally/wally/controllers/contentCreator/NewContentDialogFragment.java
+++ app/src/main/java/com/wally/wally/controllers/contentCreator/NewContentDialogFragment.java
@@ -168,7 +168,7 @@ public class NewContentDialogFragment extends TiltDialogFragment implements
         boolean isPuzzle = false;
         if (getArguments() != null) {
             mContent = (Content) getArguments().getSerializable(ARG_EDIT_CONTENT);
-            mIsEditMode = true;
+            mIsEditMode = getArguments().containsKey(ARG_EDIT_CONTENT);
             isPuzzle = getArguments().getBoolean(ARG_IS_PUZZLE, false);
         } else {
             mIsEditMode = false;
